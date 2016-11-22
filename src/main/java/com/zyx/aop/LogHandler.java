3d2d189ade9210c81007e51a3f78657affbde7ab
HL <448482470@qq.com>
2016-11-22 15:53:18 +0800

    [modify] 修改用户操作日志记录

--- src/main/java/com/zyx/aop/LogHandler.java
+++ src/main/java/com/zyx/aop/LogHandler.java
@@ -5,6 +5,7 @@ import com.zyx.mapper.LogMapper;
 import com.zyx.model.Log;
 import com.zyx.model.SysUser;
 import org.apache.shiro.SecurityUtils;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +19,7 @@ import java.util.HashMap;
 import java.util.Timer;
 
 import static java.awt.SystemColor.info;
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+import static org.apache.shiro.web.filter.mgt.DefaultFilter.user;
 
 /**
  * Created by MrDeng on 2016/11/18.
@@ -42,21 +43,30 @@ public class LogHandler {
             log.setUserId(user.getId());
             log.setCreateTime(new Date().getTime());
             log.setDel(0);
-            if (rst.contains("state=200")){
-                log.setResult(0);
-            }else {
-                log.setResult(1);
-            }
+            log.setResult(0);
             logMapper.insert(log);
         }
-        logger.info("用户操作记录"+"…………"+user.getId()+"…………操作描述："+Constants.logMap.get(methodName)+"………返回值："+rst);
+        logger.info("用户操作记录:………方法名："+methodName+"………用户id"+user.getId()+"…………操作描述："+Constants.logMap.get(methodName)+"………返回值："+rst);
         return object;
     }
 
     public void after(){
     }
 
-    public void exception(){
+    public void exception(JoinPoint point,Throwable e){
+        SysUser user = (SysUser) SecurityUtils.getSubject().getSession().getAttribute(Constants.CURRENT_USER);
+        String methodName = point.getSignature().getName();
+        if (Constants.logMap.containsKey(methodName)){
+            Log log = new Log();
+            log.setOperationDesc(Constants.logMap.get(methodName));
+            log.setUserId(user.getId());
+            log.setCreateTime(new Date().getTime());
+            log.setDel(0);
+            log.setResult(1);
+            logMapper.insert(log);
+        }
+        logger.info("用户操作记录:………方法名："+methodName+"………用户id"+user.getId()+"…………操作描述："+Constants.logMap.get(methodName)+"…………Exception:"+e);
+
     }
 
 }
